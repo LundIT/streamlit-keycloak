9985b0ca7d9f55c6eb0dcf664bf45a839261cd37
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-07-25 17:49:16 +0200

    test changes

--- streamlit_keycloak/frontend/src/Keycloak.svelte
+++ streamlit_keycloak/frontend/src/Keycloak.svelte
@@ -24,7 +24,10 @@
     }
 
     const getLoginUrl = (): string => {
-        return keycloak.createLoginUrl(loginOptions)
+        return keycloak.createLoginUrl({
+            ...loginOptions,
+            redirectUri: "redirectUri" in loginOptions ? loginOptions.redirectUri : rewritePage('/login.html'),
+        })
     }
 
     // Set up the response to Streamlit
