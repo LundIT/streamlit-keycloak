43d51bbeeecdd59c8e776bbe9ed50088fa74f2b6
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-07-25 22:12:10 +0200

    Revert "test the flow without pop up"
    This reverts commit 18613554bf8b040fb618691ab194231b829ca4c2.

--- streamlit_keycloak/frontend/src/LoginDialog.svelte
+++ streamlit_keycloak/frontend/src/LoginDialog.svelte
@@ -1,51 +1,90 @@
 <script lang="ts">
-    import { createEventDispatcher, getContext, onMount } from 'svelte'
+    import { createEventDispatcher, getContext } from 'svelte'
     import type { LabelMap } from './localization'
 
     export let loginUrl: string
-    export let redirectUri: string
 
-    const authenticate = async (): Promise<Record<string, string>> => {
-        const urlParams = new URLSearchParams(window.location.search)
-        const code = urlParams.get('code')
-        if (code) {
-            return { code }
-        } else {
-            throw new Error(labels.errorNoCode)
+    const createLoginPopup = (): void => {
+        if (currentPopup && !currentPopup.closed) {
+            currentPopup.focus()
+            return
         }
+
+        openPopup(loginUrl)
+        showPopup = true
     }
 
-    const redirectToLogin = (): void => {
-        const state = Math.random().toString(36).substring(2)
-        const loginWithRedirect = `${loginUrl}?response_type=code&client_id=your-client-id&redirect_uri=${redirectUri}&state=${state}`
-        window.location.href = loginWithRedirect
+    const openPopup = (url: string): void => {
+        const width = 400
+        const height = 600
+        const left = window.screenX + (window.innerWidth - width) / 2
+        const top = window.screenY + (window.innerHeight - height) / 2
+
+        currentPopup = window.open(
+            url,
+            'keycloak:authorize:popup',
+            `left=${left},top=${top},width=${width},height=${height},resizable,scrollbars=yes,status=1`
+        )
+    }
+
+    const runPopup = async (popup: Window): Promise<Record<string, string>> => {
+        return new Promise((resolve, reject) => {
+            // Throw exception if popup is closed manually
+            const popupTimer = setInterval(() => {
+                if (popup.closed) {
+                    window.removeEventListener(
+                        'message',
+                        popupEventListener,
+                        false
+                    )
+                    clearInterval(popupTimer)
+
+                    reject(new Error(labels.errorPopupClosed))
+                }
+            }, 1000)
+
+            // Wait for postMessage from popup if login is successful
+            const popupEventListener = function (event: MessageEvent): void {
+                if (event.origin !== window.location.origin) return
+                if (!Object.keys(event.data).includes('code')) return
+
+                window.removeEventListener('message', popupEventListener, false)
+                clearInterval(popupTimer)
+
+                popup.close()
+                resolve(event.data)
+            }
+
+            window.addEventListener('message', popupEventListener)
+        })
+    }
+
+    const authenticateWithPopup = async (
+        popup: Window | null
+    ): Promise<void> => {
+        if (!popup) {
+            throw new Error(labels.errorNoPopup)
+        }
+
+        await runPopup(popup)
+        dispatch('loggedin')
     }
 
     const labels: LabelMap = getContext('localization')
     const dispatch = createEventDispatcher()
 
-    let showError = false
-    let errorMessage = ''
-
-    onMount(async () => {
-        try {
-            const result = await authenticate()
-            if (result && result.code) {
-                dispatch('loggedin')
-            }
-        } catch (error) {
-            showError = true
-            errorMessage = error.message
-        }
-    })
+    let currentPopup: Window | null
+    let showPopup = false
 </script>
 
 <div class="alert alert-warning" on:loggedin>
-    <button type="button" class="btn btn-primary" on:click={redirectToLogin}>
+    <button type="button" class="btn btn-primary" on:click={createLoginPopup}>
         <span>{labels.labelButton}</span>
     </button>
     <span class="mx-3">{labels.labelLogin}</span>
-    {#if showError}
-        <div class="alert alert-danger mt-3 mb-0">{errorMessage}</div>
+    {#if showPopup}
+        {#await authenticateWithPopup(currentPopup) catch error}
+            <div class="alert alert-danger mt-3 mb-0">{error.message}</div>
+        {/await}
     {/if}
 </div>
