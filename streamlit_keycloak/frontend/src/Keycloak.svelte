5b4dc0131a1c493f9d3e4c53675f3a11fea7e64f
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-07-29 13:10:48 +0200

    test changes

--- streamlit_keycloak/frontend/src/Keycloak.svelte
+++ streamlit_keycloak/frontend/src/Keycloak.svelte
@@ -113,7 +113,9 @@
                 loginUrl={getLoginUrl()}
                 isAuthenticating={isAuthenticating}
                 on:loggedin={() => {
+                    isAuthenticating = true
                     keycloak.login(loginOptions)
+                    isAuthenticating = false
                 }}
             />
         {:else}
